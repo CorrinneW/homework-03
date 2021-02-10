9e3c9ee7ca842e1ac6628261964535a792b84b3b
Corrinne W <corrinneworden@gmail.com>
2021-02-10 18:59:56 -0500

    wrote button functions to open and close password settings popup

--- script.js
+++ script.js
@@ -1,26 +1,36 @@
 // Assignment Code
-const password = document.querySelector("#password")
 
 //buttons
-const generateBtn = document.querySelector("#generate");
-const okayBtn = document.querySelector("#okayBtn")
+const generateBtn = document.querySelector("#generate"); //opens popup
+const okayBtn = document.querySelector("#okayBtn") //closes popup
 
 //settings
+const passwordSettings = document.querySelector(".passwordSettings");
 const lengthEl = document.querySelector("#length");
 const uppercaseEl = document.querySelector("#uppercase");
 const lowercaseEl = document.querySelector("#lowercase");
 const numberEl = document.querySelector("#number");
 const symbolEl = document.querySelector("#symbol");
 
-// Write password to the #password input
-function writePassword() {
-  const password = generatePassword();
-  const passwordText = document.querySelector("#password");
+//button functions
 
-  passwordText.value = password;
-
+generateBtn.onclick = function() {
+  passwordSettings.style.display = "block";
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+okayBtn.onclick = function() {
+  passwordSettings.style.display = "none";
+}
+
+
+// // Write password to the #password input
+// function writePassword() {
+//   const password = generatePassword();
+//   const passwordText = document.querySelector("#password");
+//   passwordText.value = password;
+
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
 
