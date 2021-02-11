e9a76abddec47d0453dcd57ac4b411a8b4375933
Corrinne W <corrinneworden@gmail.com>
2021-02-11 12:19:03 -0500

    added randomization functions, function to differentiate between checked and unchecked values, and a filter to ignore unchecked values

--- script.js
+++ script.js
@@ -12,6 +12,44 @@ const lowercaseEl = document.querySelector("#lowercase");
 const numberEl = document.querySelector("#number");
 const symbolEl = document.querySelector("#symbol");
 
+//object contains randomization functions
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol
+};
+
+//generates a random character using corresponding character codes
+function getRandomLower() {
+  returnString.fromCharCode(
+    Math.floor(
+      Math.random() * 26) + 97
+    )
+  )
+}
+
+function getRandomUpper() {
+  returnString.fromCharCode(
+    Math.floor(
+      Math.random() * 26) + 65
+    )
+  )
+}
+
+function getRandomNumber() {
+  returnString.fromCharCode(
+    Math.floor(
+      Math.random() * 10) + 48
+    )
+  )
+}
+
+function getRandomSymbol() {
+  const symbols = "~`!@#$%^&*()_-+={[}]|\:;<,>";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
 //button functions: open & close popup
 generateBtn.onclick = function() {
   passwordSettings.style.display = "block";
@@ -22,16 +60,50 @@ okayBtn.onclick = function() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword, () => {
+  // includes checked values in generated password and converts length entry from string to number
+  const length = +lengthEl.value;
+  const hasLower = lowercaseEl.checked;
+  const hasUpper = uppercaseEl.checked;
+  const hasNumber = numberEl.checked;
+  const hasSymbol = symbolEl.checked;
+
+  password.innerText = generatePassword(length, hasLower, hasUpper, hasNumber, hasSymbol);
+});
+
+//generate password with selected options
+
+function generatePassword(length, lower, upper, number, symbol) {
+  //sets password to an empty string
+  let generatePassword = '';
+
+  //counts number of checked values
+  const typesCount = lower + upper + number + symbol; 
+  
+  //curly braces convert array to true/false key. Filter ignores all falsey (unchecked) items
+  const typesArr = [{lower} {upper} {number} {symbol}].filter
+  (item => Object.values(item)[0]); //0 represents false (unchecked) here
+
+  //if no box is checked, generatePassword returns an empty string
+  if(typesCount === 0) {return '';}
+
+  //loops generatePassword til number of characters matches password length
+  for (let i = 0; i < length; i += typesCount) {
+    typesArr.forEach(type => {
+      const funcName = Object.keys(type) [0];
+      generatedPassword += randomFunc[funcName]();
+    })
+  }
+}
 
 
 // // Write password to the #password input
-// function writePassword() {
-//   const password = generatePassword();
-//   const passwordText = document.querySelector("#password");
-//   passwordText.value = password;
+function writePassword() {
+  const password = generatePassword(length);
+  const passwordText = document.querySelector("#password");
+  passwordText.value = password;
 
-// }
+}
 
 
 
