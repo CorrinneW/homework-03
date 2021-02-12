803ba67f3d126b409d4e1b32ad9cf56151dd1c59
Corrinne W <corrinneworden@gmail.com>
2021-02-11 22:58:27 -0500

    reworking javascript file

--- script.js
+++ script.js
@@ -1,109 +1,31 @@
 // Assignment Code
+const generateBtn = document.querySelector("#generate");
 
-//buttons
-const generateBtn = document.querySelector("#generate"); //opens popup
-const okayBtn = document.querySelector("#okayBtn") //closes popup
+//password settings
+let lengthEl;
+let lowercaseEl; 
+let uppercaseEl;
+let numberEl;
+let symbolEl;
 
-//settings
-const passwordSettings = document.querySelector(".passwordSettings");
-const lengthEl = document.querySelector("#length");
-const uppercaseEl = document.querySelector("#uppercase");
-const lowercaseEl = document.querySelector("#lowercase");
-const numberEl = document.querySelector("#number");
-const symbolEl = document.querySelector("#symbol");
+// Write password to the #password input
+function writePassword() {
+  const password = generatePassword();
+  const passwordText = document.querySelector("#password");
 
-//generates a random character using corresponding character codes
-function getRandomLower() {
-  returnString.fromCharCode(
-    Math.floor(
-      Math.random() * 26) + 97
-    );
+  passwordText.value = password;
 }
-
-function getRandomUpper() {
-  returnString.fromCharCode(
-    Math.floor(
-      Math.random() * 26) + 65
-    );
-}
-
-function getRandomNumber() {
-  returnString.fromCharCode(
-    Math.floor(
-      Math.random() * 10) + 48
-    );
-}
-
-function getRandomSymbol() {
-  const symbols = "~`!@#$%^&*()_-+={[}]|\:;<,>";
-  return symbols[Math.floor(Math.random() * symbols.length)];
-}
-
-//object contains randomization functions
-const randomFunc = {
-  lower: getRandomLower,
-  upper: getRandomUpper,
-  number: getRandomNumber,
-  symbol: getRandomSymbol
-};
-
-//button functions: open & close popup
-// generateBtn.onclick = function() {
-//   passwordSettings.style.display = "block";
-// }
-
-// okayBtn.onclick = function() {
-//   passwordSettings.style.display = "none";
-// }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword, (length) => {
-  // includes checked values in generated password and converts length entry from string to number
-  const length = +lengthEl.value;
-  const hasLower = lowercaseEl.checked;
-  const hasUpper = uppercaseEl.checked;
-  const hasNumber = numberEl.checked;
-  const hasSymbol = symbolEl.checked;
+generateBtn.addEventListener("click", writePassword);
 
-  password.innerText = generatePassword(length, hasLower, hasUpper, hasNumber, hasSymbol);
-});
 
-//generate password with selected options
-
-function generatePassword(length, lower, upper, number, symbol) {
-  //sets password to an empty string
-  let generatedPassword = '';
-
-  //counts number of checked values
-  const typesCount = lower + upper + number + symbol; 
-  
-  //curly braces convert array to true/false key. Filter ignores all falsey (unchecked) items
-  const typesArr = [{lower} {upper} {number} {symbol}].filter
-  (item => Object.values(item)[0]); //0 represents false (unchecked) here
-
-  //if no box is checked, generatePassword returns an empty string
-  if(typesCount === 0) {return '';}
-
-  //loops generatePassword til number of characters matches password length
-  for (let i = 0; i < length; i += typesCount) {
-    typesArr.forEach(type => {
-      const funcName = Object.keys(type) [0];
-      generatedPassword += randomFunc[funcName]();
-    })
-    
-  }
+function generatePassword() {
+  //prompts for password settings
+  lengthEl = parseInt(prompt("Enter a number between 8 and 128."));
+  lowercaseEl = confirm("Include lowercase letters?");
+  uppercaseEl = confirm("Include uppercase letters?");
+  numberEl = confirm("Include numbers?");
+  symbolEl = confirm("Include symbols?");
 }
-
-
-// // Write password to the #password input
-function writePassword() {
-  const password = generatePassword(length);
-  const passwordText = document.querySelector("#password");
-  passwordText.value = password;
-
-  //displays password on screen
-  console.log generatedPassword;
-}
-
-
 
